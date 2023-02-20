446fad5825624a12f891ce4970bfd680d17e4841
Madhur <madhur-prashant.bhandari@students.mq.edu.au>
2023-02-20 20:17:26 +1100

    Added first step for Data View

--- src/main/java/com/curioushead/controller/RawDataViewController.java
+++ src/main/java/com/curioushead/controller/RawDataViewController.java
@@ -3,20 +3,29 @@ package com.curioushead.controller;
 import com.curioushead.model.RawDataViewModel;
 import com.curioushead.repository.DataViewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 public class RawDataViewController {
     private final DataViewRepository dataViewRepository;
+
     @Autowired
     public RawDataViewController(DataViewRepository dataViewRepository) {
         this.dataViewRepository = dataViewRepository;
     }
 
+    @GetMapping(value = "/raw-data-view")
+    public Optional<RawDataViewModel> getRawDataView(@RequestBody RawDataViewModel dataViewModel) {
+        return dataViewRepository.findById(dataViewModel.getName());
+    }
+
     @PostMapping(value = "/raw-data-view")
-    public String addUser(@RequestBody RawDataViewModel dataViewModel){
+    public String addRawDataView(@RequestBody RawDataViewModel dataViewModel) {
         dataViewRepository.save(dataViewModel);
         return "Data View created Successfully!!!";
     }
