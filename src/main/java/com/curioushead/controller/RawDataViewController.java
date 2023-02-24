34b0c663236d7268f4b67706ecceedafb78c3ad9
Madhur <madhur-prashant.bhandari@students.mq.edu.au>
2023-02-24 20:55:21 +1100

    Added first step for Data View

--- src/main/java/com/curioushead/controller/RawDataViewController.java
+++ src/main/java/com/curioushead/controller/RawDataViewController.java
@@ -3,30 +3,41 @@ package com.curioushead.controller;
 import com.curioushead.model.RawDataViewModel;
 import com.curioushead.repository.DataViewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
+
+import org.apache.commons.lang3.StringUtils;
 
 @RestController
 public class RawDataViewController {
     private final DataViewRepository dataViewRepository;
+
+    final String INVALID = "Invalid property!!!";
+    final String EXISTS = "Data View already exists!!!";
+    final String VALID = "Data View created Successfully!!!";
 
     @Autowired
     public RawDataViewController(DataViewRepository dataViewRepository) {
         this.dataViewRepository = dataViewRepository;
     }
 
-    @GetMapping(value = "/raw-data-view")
+    @GetMapping(value = "/data-view-service")
     public Optional<RawDataViewModel> getRawDataView(@RequestBody RawDataViewModel dataViewModel) {
         return dataViewRepository.findById(dataViewModel.getName());
     }
 
-    @PostMapping(value = "/raw-data-view")
+    @PostMapping(value = "/data-view-service")
     public String addRawDataView(@RequestBody RawDataViewModel dataViewModel) {
-        dataViewRepository.save(dataViewModel);
-        return "Data View created Successfully!!!";
+
+        if (dataViewRepository.existsById(dataViewModel.getName())) {
+            return EXISTS;
+        }
+        if (StringUtils.isNotEmpty(dataViewModel.getSchema())) {
+            dataViewRepository.save(dataViewModel);
+            return VALID;
+        } else {
+            return INVALID;
+        }
     }
 }
