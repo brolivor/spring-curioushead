993e4de6bfd8af788017c7ca2578a496c7625315
Madhur <madhur-prashant.bhandari@students.mq.edu.au>
2023-03-04 18:11:31 +1100

    Refactoring

--- src/main/java/com/curioushead/controller/DataViewController.java
+++ src/main/java/com/curioushead/controller/DataViewController.java
@@ -1,6 +1,6 @@
 package com.curioushead.controller;
 
-import com.curioushead.model.RawDataViewModel;
+import com.curioushead.model.DataViewModel;
 import com.curioushead.repository.DataViewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -10,7 +10,7 @@ import java.util.Optional;
 import org.apache.commons.lang3.StringUtils;
 
 @RestController
-public class RawDataViewController {
+public class DataViewController {
     private final DataViewRepository dataViewRepository;
 
     final String INVALID = "Invalid property!!!";
@@ -18,17 +18,17 @@ public class RawDataViewController {
     final String VALID = "Data View created Successfully!!!";
 
     @Autowired
-    public RawDataViewController(DataViewRepository dataViewRepository) {
+    public DataViewController(DataViewRepository dataViewRepository) {
         this.dataViewRepository = dataViewRepository;
     }
 
     @GetMapping(value = "/data-view-service")
-    public Optional<RawDataViewModel> getRawDataView(@RequestBody RawDataViewModel dataViewModel) {
+    public Optional<DataViewModel> getRawDataView(@RequestBody DataViewModel dataViewModel) {
         return dataViewRepository.findById(dataViewModel.getName());
     }
 
     @PostMapping(value = "/data-view-service")
-    public String addRawDataView(@RequestBody RawDataViewModel dataViewModel) {
+    public String addRawDataView(@RequestBody DataViewModel dataViewModel) {
 
         if (dataViewRepository.existsById(dataViewModel.getName())) {
             return EXISTS;
