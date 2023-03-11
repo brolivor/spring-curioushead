8476a059b21ca12303d8dbc74db227cd9f2ce764
madhur <madhur-prashant.bhandari@students.mq.edu.au>
2023-03-11 20:49:28 +1100

    Created new module for Miscellaneous Services

--- scripts/refresh_deployment.sh
+++ scripts/refresh_deployment.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-kubectl delete services spring-curioushead-root-project-service > /dev/null 2>&1
-kubectl delete deployment spring-curioushead-root-project > /dev/null 2>&1
+kubectl delete services spring-curioushead-miscellaneous-service > /dev/null 2>&1
+kubectl delete services spring-curioushead-profiles-service > /dev/null 2>&1
+
+kubectl delete deployment spring-curioushead-miscellaneous > /dev/null 2>&1
+kubectl delete deployment spring-curioushead-profiles > /dev/null 2>&1
